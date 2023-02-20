be1d6508386b8a6893cda827ba521e628221f3f8
zcdx <gosha-saf@yandex.ru>
2023-02-20 23:12:04 +0300

    feat: onitamapiece inicialization

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Game : MonoBehaviour
 {
+    public GameObject onitamaPiece;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(onitamaPiece, new Vector3(0, 0, -1), Quaternion.identity);
     }
 
   
---
+++ Assets/Scripts/OnitamaPiece.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnitamaMan : MonoBehaviour
+{
+    public GameObject controller;
+    public GameObject movePlate;
+}
